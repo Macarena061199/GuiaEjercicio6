f86af70773eeed3e87142763c21edc1759940fd1
Macarena061199 <102336411+Macarena061199@users.noreply.github.com>
2022-04-02 17:41:48 -0300

    Listo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ConsoleKeyInfo key;
+Console.TreatControlCAsInput = true;
+Console.WriteLine("Por favor presione cualquier tecla. ");
+Console.WriteLine("Si desea salir presione Shift+Ctrl+F.");
+
+do
+{
+    key = Console.ReadKey();
+    Console.WriteLine();
+
+    if ((key.Key == ConsoleKey.F) &&
+        ((key.Modifiers & ConsoleModifiers.Control) != 0) && ((key.Modifiers & ConsoleModifiers.Shift) != 0))
+    {
+        break;
+    }
+
+} while (true);
